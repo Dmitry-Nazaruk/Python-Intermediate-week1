3aa3e0ab3e1aab04a99abe88cf9c388efa4490bd
Dmitry-Nazaruk <12nazaruk@gmail.com>
2022-05-26 20:39:42 +0300

    The functions that are responsible for mathematical operations now return an object of the fraction class, instead of a float

--- w1.py
+++ w1.py
@@ -2,23 +2,27 @@ class FractionMixin:
 
     @staticmethod
     def add(fr1, fr2):
-        result = (fr1.num / fr1.den) + (fr2.num / fr2.den)
-        return result
+        new_den = fr1.den * fr2.den
+        new_num = fr1.num * fr2.den + fr1.num * fr2.den
+        return Fraction(new_num, new_den)
 
     @staticmethod
     def sub(fr1, fr2):
-        result = (fr1.num / fr1.den) - (fr2.num / fr2.den)
-        return result
+        new_den = fr1.den * fr2.den
+        new_num = fr1.num * fr2.den - fr2.num * fr1.den
+        return Fraction(new_num, new_den)
 
     @staticmethod
     def mul(fr1, fr2):
-        result = (fr1.num / fr1.den) * (fr2.num / fr2.den)
-        return result
+        new_den = fr1.den * fr2.den
+        new_num = fr1.num * fr2.num
+        return Fraction(new_num, new_den)
 
     @staticmethod
     def div(fr1, fr2):
-        result = (fr1.num / fr1.den) / (fr2.num / fr2.den)
-        return result
+        new_den = fr1.den * fr2.num
+        new_num = fr1.num * fr2.den
+        return Fraction(new_num, new_den)
 
 
 class Fraction(FractionMixin):
@@ -36,20 +40,24 @@ class Fraction(FractionMixin):
         return self.__den
 
     def __add__(self, other):
-        result = (self.num / self.den) + (other.num / other.den)
-        return result
+        new_den = self.den * other.den
+        new_num = self.num * other.den + other.num * self.den
+        return Fraction(new_num, new_den)
 
     def __sub__(self, other):
-        result = (self.num / self.den) - (other.num / other.den)
-        return result
+        new_den = self.den * other.den
+        new_num = self.num * other.den - other.num * self.den
+        return Fraction(new_num, new_den)
 
     def __mul__(self, other):
-        result = (self.num / self.den) * (other.num / other.den)
-        return result
+        new_den = self.den * other.den
+        new_num = self.num * other.num
+        return Fraction(new_num, new_den)
 
     def __truediv__(self, other):
-        result = (self.num / self.den) / (other.num / other.den)
-        return result
+        new_den = self.den * other.num
+        new_num = self.num * other.den
+        return Fraction(new_num, new_den)
 
     @classmethod
     def from_string(cls, str_value):
@@ -63,13 +71,13 @@ class Fraction(FractionMixin):
 
 fr1 = Fraction(10, 20)
 fr2 = Fraction.from_string('20,10')
-print(fr1.div(fr1, fr2))
-print(fr1.add(fr1, fr2))
-print(fr1.mul(fr1, fr2))
-print(fr1.sub(fr1, fr2))
-print(fr1 + fr2)
-print(fr1 - fr2)
-print(fr1 * fr2)
-print(fr1 / fr2)
-print(fr1.num, fr2.den)
-print(fr1)
+print('fr1.div(fr1, fr2): ', fr1.div(fr1, fr2))
+print('fr1.add(fr1, fr2): ', fr1.add(fr1, fr2))
+print('fr1.mul(fr1, fr2): ', fr1.mul(fr1, fr2))
+print('fr1.sub(fr1, fr2): ', fr1.sub(fr1, fr2))
+print('fr1 + fr2: ', fr1 + fr2)
+print('fr1 - fr2: ', fr1 - fr2)
+print('fr1 * fr2: ', fr1 * fr2)
+print('fr1 / fr2: ', fr1 / fr2)
+print('fr1.num, fr2.den: ', fr1.num, fr2.den)
+print('fr1: ', fr1)
